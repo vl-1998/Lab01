efb72c6255ce5762378ae564c1ae865e9520bab6
sabinoMac13 <sabinoMac13@192.168.1.107>
2020-03-11 15:32:25 +0100

    Esercizio svolto con LINKEDLIST

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -3,6 +3,7 @@ package it.polito.tdp.parole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,7 +13,9 @@ import javafx.scene.control.TextField;
 
 public class FXMLController {
 	
+	private String parolaInserita;
 	Parole elenco ;
+	private long tempoImpiegato;
 
     @FXML
     private ResourceBundle resources;
@@ -31,15 +34,92 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private TextArea txtTime;
 
+    
+    @FXML
+    private Button btnCancel;
+
+    public boolean check (String p) {
+		boolean check = true;
+		for (int i=0; i<p.length();i++) {
+			if (!Character.isLetter(p.charAt(i))){
+				check=false;
+			}
+		}
+		return check;
+	}
+    
     @FXML
     void doInsert(ActionEvent event) {
     	// TODO
+    	//ripulisco il txt dei risultati prima di iniziare per evitare che mi ristampi piu volte la stessa cosa
+    	txtResult.clear();
+    	//prendo la parola inserita dall'utente
+    	this.parolaInserita = txtParola.getText();
+    	//controllo che l'utente non abbia inserito una parola vuota (ma non pulisco l'elenco in modo che non dimentichi eventuali parole gia inserite)
+    	if (parolaInserita.length()==0) {
+    		txtResult.clear();
+    		txtResult.appendText("Inserimento non valido \n");
+        	txtParola.clear();
+
+    		return;
+    	}
+    	
+    	//effettuo il controllo nel caso in cui l'utente avesse inserito dei caratteri non validi
+    	if (this.check(parolaInserita)==false) {
+    		txtResult.clear();
+    		txtResult.appendText("Inserire parola valida \n");
+        	txtParola.clear();
+
+    		return;
+    	}
+    	
+    	//aggiungo nell'elenco la parola nuova
+    	elenco.addParola(parolaInserita);
+    	
+    	//stampo la parola nel txt del risultato in ordine alfabetico grazie a getElenco
+    	for (String s: elenco.getElenco()) {
+    		txtResult.appendText(s+"\n");
+    	}	
+    	
+    	this.doTime(event);
+    	    	
+    	txtParola.clear();
     }
 
     @FXML
+    public void doTime (ActionEvent event) {
+    	long tempo=System.nanoTime();
+    	txtTime.appendText(String.valueOf(tempo)+"\n");
+    }
+    
+    @FXML
     void doReset(ActionEvent event) {
     	// TODO
+    	this.elenco.reset();
+    	txtResult.clear();
+    	txtTime.clear();
+    }
+    
+    @FXML
+    void doCancel(ActionEvent event) {
+    	
+    	String parolaDaEliminare="";
+    	parolaDaEliminare = txtResult.getSelectedText();
+    	
+    	if (parolaDaEliminare.compareTo("")!=0) {
+    	elenco.cancelWord(parolaDaEliminare);
+    	txtResult.clear();
+    	
+    	for (String s: elenco.getElenco()) {
+    		txtResult.appendText(s+"\n");
+    	}	
+    	
+    	this.doTime(event);
+    	}
     }
 
     @FXML
@@ -47,8 +127,10 @@ public class FXMLController {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'Scene.fxml'.";
+        elenco = new Parole();
 
-        elenco = new Parole() ;
     }
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,50 @@
 package it.polito.tdp.parole.model;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
-		
+	LinkedList <String> elenco = new LinkedList <String>();
+
 	public Parole() {
 		//TODO
 	}
 	
 	public void addParola(String p) {
+			elenco.add(p);
 		//TODO
 	}
 	
 	public List<String> getElenco() {
 		//TODO
-		return null;
+		LinkedList <String> lTemp = elenco;
+		Collections.sort(lTemp, new Comparator <String>(){
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				
+				return o1.compareTo(o2);
+			}
+		});
+			
+		return lTemp;
+	}
+	
+	public void cancelWord (String p) {
+		for (String s : elenco) {
+			if (s!=null) {
+				if (s.compareTo(p)==0) {
+					elenco.remove(s);
+				}
+			}
+		}	
 	}
 	
 	public void reset() {
 		// TODO
+		elenco.removeAll(elenco);
 	}
 
 }
